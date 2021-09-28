ccb3c485428bd2cb914d55dde0a58b75bea5b8ad
amarsaahu <117amarnath@gmail.com>
2021-09-28 12:09:52 +0530

    cleaned up

--- src/App.js
+++ src/App.js
@@ -3,42 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    <div classname="leftcontainer">
-    <div className="homepage">
-    <div className="name-description">
-      <p>Glad you are here!</p>
-      <p className="name">AMARNATH SATHANI</p>
-   
-    </div>
-    <div classname="contact-us">
-      <div className="login-form">
-    <input type="text" placeholder="Username" width="339px" />
-    <div className="homepage1">
-      <div className="pwd">
-        <input type="password" placeholder="password" /></div>
-    
-    <span>*</span>
-
-    </div>
-    <div classname="Login-btn"> 
-    <button>Log In </button>
-    </div>
-    
-
-      </div>
-      <div className="signup-btn">
-        <button>Create new account</button>
-
-      </div>
-
-    </div>
-    </div>
-    </div>
-    
-    
-    </>
-
+  <div>
+    Amarnath
+  </div>
   
   );
 }
